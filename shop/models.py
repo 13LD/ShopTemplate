d72afad5ee871ca659f30a81a081a213e029c84f
Dmitry Lysogor <tompla96@ukr.net>
2016-07-29 13:03:09 +0300

    refactor files and links on them

--- shop/models.py
+++ shop/models.py
@@ -7,6 +7,17 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
     quantity = models.IntegerField(default=0)
     price = models.CharField(max_length=45)
 
+class User(models.Model):
+    userName = models.CharField(max_length=50)
+    userSurname = models.CharField(max_length=50)
+    userAge = models.DateField()
+
+class Sales(models.Model):
+    user = models.ForeignKey(User)
+    product = models.ForeignKey(Product)
+    saleType = models.CharField(max_length=50)
+    saleDate = models.CharField(max_length=50)
