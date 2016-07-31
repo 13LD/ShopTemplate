205340ed15ce39c68c00eff72794248bb7a46af3
Dmitry Lysogor <tompla96@ukr.net>
2016-07-31 16:49:05 +0300

    added navigation pane

--- shop/models.py
+++ shop/models.py
@@ -14,6 +14,7 @@ class Product(models.Model):
 class User(models.Model):
     userName = models.CharField(max_length=50)
     userSurname = models.CharField(max_length=50)
+    userPasswoer = models.CharField(max_length=50)
     userAge = models.DateField()
 
 class Sales(models.Model):
